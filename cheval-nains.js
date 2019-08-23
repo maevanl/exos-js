767b1ed2650bab899f69d06355fee1157e6da6ed
Maëva <52455242+maevanl@users.noreply.github.com>
2019-08-23 13:13:23 +0200

    Create cheval-nains.js

---
+++ cheval-nains.js
@@ -0,0 +1,14 @@
+while (true) {
+    var cheval = prompt("Quelle est la couleur du cheval blanc d'Henri IV?");
+    if (cheval === 'blanc') {
+        break;
+    }
+    alert("N'importe quoi! Réessaye!");
+}
+var nain = prompt("Combien y a-t-il de sept nains?");
+  if(nain === '7'){
+    alert("bravo!")
+  }
+  else{
+    alert("Réfléchis un peu...")
+  }
